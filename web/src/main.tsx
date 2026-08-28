0b4491dbd01b6540f0bad37129a95d23edb71970
luisaherrera0264 <luluherreracoelho0264@gmail.com>
2026-08-28 00:45:17 +0000

    novocommit

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Prince from './jiraia.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    < Prince/>
+    <App />
   </StrictMode>,
 )
